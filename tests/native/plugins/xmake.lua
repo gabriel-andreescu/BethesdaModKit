5e43d3ac30b75b25b1d18668a2de34e03abbbcd8
Gabriel Andreescu <gabriel@andreescu.dev>
2026-09-19 12:44:04 +0300

    style(xmake): preserve target block indentation

--- tests/native/plugins/xmake.lua
+++ tests/native/plugins/xmake.lua
@@ -22,17 +22,18 @@ else
     add_requires("commonlibf4 2026.09.13")
 end
 
-target("TestPlugin")
-set_version("0.1.0")
-if is_config("game", "skyrim") then
-    add_rules("@commonlibsse-ng/plugin", { author = "Test Author", description = "Plugin package test" })
-    add_rules("@addon/bmk/skyrim.plugin")
-    add_rules("@devbench-api/integration")
-    add_packages("bmk", "commonlibsse-ng", "devbench-api", { external = true })
-    add_files("skyrim.cpp")
-else
-    add_rules("@commonlibf4/plugin", { author = "Test Author", description = "Plugin package test" })
-    add_rules("@addon/bmk/fallout4.plugin")
-    add_packages("commonlibf4", { external = true })
-    add_files("fallout4.cpp")
-end
+target("TestPlugin", function()
+    set_version("0.1.0")
+    if is_config("game", "skyrim") then
+        add_rules("@commonlibsse-ng/plugin", { author = "Test Author", description = "Plugin package test" })
+        add_rules("@addon/bmk/skyrim.plugin")
+        add_rules("@devbench-api/integration")
+        add_packages("bmk", "commonlibsse-ng", "devbench-api", { external = true })
+        add_files("skyrim.cpp")
+    else
+        add_rules("@commonlibf4/plugin", { author = "Test Author", description = "Plugin package test" })
+        add_rules("@addon/bmk/fallout4.plugin")
+        add_packages("commonlibf4", { external = true })
+        add_files("fallout4.cpp")
+    end
+end)
--- tests/native/xmake.lua
+++ tests/native/xmake.lua
@@ -5,14 +5,15 @@ add_rules("mode.releasedbg")
 add_rules("plugin.compile_commands.autoupdate", { outputdir = ".", lsp = "clangd" })
 add_requires("catch2 3.8.1")
 
-target("InspectionTaskTests")
-set_kind("binary")
-add_files("skyrim/InspectionTaskTests.cpp")
-add_includedirs("../../native/include")
-add_packages("catch2")
-add_tests("inspection")
-after_config(function(target)
-    if target:has_tool("cxx", "cl") then
-        target:add("symbols", "embed")
-    end
+target("InspectionTaskTests", function()
+    set_kind("binary")
+    add_files("skyrim/InspectionTaskTests.cpp")
+    add_includedirs("../../native/include")
+    add_packages("catch2")
+    add_tests("inspection")
+    after_config(function(target)
+        if target:has_tool("cxx", "cl") then
+            target:add("symbols", "embed")
+        end
+    end)
 end)
